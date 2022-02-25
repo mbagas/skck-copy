a4abcb02ca94f527701c09f88e38d72b611c7045
KRSBX <firdaus.sati51@gmail.com>
2022-02-25 22:59:18 +0700

    chore: init users model

---
+++ api/src/migrations/20220225101747-create-guru.js
@@ -0,0 +1,38 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Gurus', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      jabatan: {
+        type: Sequelize.STRING,
+      },
+      alamat: {
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Gurus');
+  },
+};
---
+++ api/src/utils/constants.js
@@ -0,0 +1,6 @@
+exports.USER_ROLE = {
+  ADMIN: 'admin',
+  GURU: 'guru',
+  SISWA: 'siswa',
+  ORANG_TUA: 'orang_tua',
+};
