818d2e144c1d3fd03ee07f44fb15b337798134ed
KRSBX <firdaus.sati51@gmail.com>
2022-03-05 21:06:33 +0700

    chore: init chakra ui

---
+++ app/src/utils/customTheme.ts
@@ -0,0 +1,35 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  color: {
+    royalBlue: {
+      100: '#4F72D9',
+      200: '#184ADE',
+    },
+    royalRed: {
+      100: '#E64839',
+      200: '#B91D0E',
+    },
+    royalYellow: {
+      100: '#F4C33D',
+      200: '#D69F08',
+    },
+    royalGreen: {
+      100: '#1ECA8B',
+      200: '#00965F',
+    },
+    royalOrange: {
+      100: '#F6924F',
+      200: '#FF6600',
+    },
+    royalGray: {
+      100: '#C4C4C4',
+      200: '#3F4143',
+    },
+    royalBlack: {
+      100: '#C4C4C4',
+    },
+  },
+});
+
+export default theme;
