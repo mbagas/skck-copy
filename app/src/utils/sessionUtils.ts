f0621ace248bd302b89bf2c8cd05791300bf2eba
KRSBX <firdaus.sati51@gmail.com>
2022-03-20 03:04:38 +0700

    fix: custom color variant

--- app/src/utils/sessionUtils.ts
+++ app/src/utils/sessionUtils.ts
@@ -2,17 +2,18 @@ import jwtDecode, { JwtPayload } from 'jwt-decode';
 import _ from 'lodash';
 import { RoleType } from './interface';
 
-interface IJWT extends JwtPayload {
+interface IJWT extends Omit<JwtPayload, 'exp'> {
   id: number;
   accountId: number;
   role: RoleType;
+  exp: number;
 }
 
 export const setToken = (token: string) => localStorage.setItem('access_token', token);
 
-export const getToken = () => localStorage.getItem('access_token');
+export const getToken = (): string | null => localStorage.getItem('access_token');
 
-export const getRole = () => {
+export const getRole = (): RoleType | undefined => {
   const token = getToken();
 
   if (_.isNil(token)) return;
@@ -20,6 +21,22 @@ export const getRole = () => {
   const { role } = jwtDecode<IJWT>(token);
 
   return role;
+};
+
+export const isExpired = (): boolean => {
+  const token = getToken();
+
+  if (_.isNil(token)) return true;
+
+  const { exp } = jwtDecode<IJWT>(token);
+
+  return exp * 1000 < Date.now();
+};
+
+export const isAuthenticated = (): boolean => {
+  const token = localStorage.getItem('token');
+
+  return !token && !isExpired();
 };
 
 const SessionUtils = {
