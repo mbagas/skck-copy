bf8774e4680a3ad2acaa4c09bbbca114fcac1d04
KRSBX <firdaus.sati51@gmail.com>
2022-04-03 23:51:29 +0700

    feat: dummy seeder

--- api/src/seeders/20220316082943-siswa-seeder.js
+++ api/src/seeders/20220316082943-siswa-seeder.js
@@ -7,37 +7,47 @@ const {
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const orangTuaUsers = await Users.bulkCreate([
-      {
-        userName: 'orangTua',
-        password: await hashText('orangTua'),
+    const orangTuaUsers = await Users.bulkCreate(
+      _.map(Array(3), async (val, index) => ({
+        userName: `orangTua${index + 1}`,
+        password: await hashText(`orangTua${index + 1}`),
         role: USER_ROLE.ORANG_TUA,
-      },
-    ]);
+      }))
+    );
 
     const orangTuas = await OrangTuas.bulkCreate(
-      _.map(orangTuaUsers, (orangTua) => ({
-        namaLengkap: 'orangTua',
-        alamat: 'alamat',
+      _.map(orangTuaUsers, (orangTua, index) => ({
+        namaLengkap: `orangTua${index + 1}`,
+        alamat: `alamat${index + 1}`,
         userId: orangTua.id,
       }))
     );
 
-    const users = await Users.bulkCreate([
-      {
-        userName: 'siswa',
-        password: await hashText('siswa'),
+    const users = await Users.bulkCreate(
+      _.map(Array(9), async (value, index) => ({
+        userName: `siswa${index + 1}`,
+        password: await hashText(`siswa${index + 1}`),
         role: USER_ROLE.SISWA,
-      },
-    ]);
+      }))
+    );
+
+    let counter = 0;
 
     await Siswas.bulkCreate(
-      _.map(users, (user, index) => ({
-        namaLengkap: 'namaLengkap',
-        alamat: 'alamat',
-        userId: user.id,
-        orangTuaId: orangTuas[index].id,
-      }))
+      _.map(users, (user, index) => {
+        const obj = {
+          namaLengkap: `namaLengkap${index + 1}`,
+          nis: _.map(Array(9), (val, id) => `${id + 1}`).join(''),
+          nisn: _.map(Array(9), (val, id) => `${id + 2}`).join(''),
+          alamat: 'alamat',
+          userId: user.id,
+          orangTuaId: orangTuas[counter].id,
+        };
+
+        if ((index + 1) % 3 === 0) counter += 1;
+
+        return obj;
+      })
     );
   },
 
